64bfc82215697c7f7c4dda05f5f2fb95647df001
Tseera <tayseersomane@gmail.com>
2024-04-23 14:00:34 +0300

    changes

---
+++ src/Components/Count.jsx
@@ -0,0 +1,62 @@
+
+
+import React from 'react'
+import { useState } from 'react';
+
+function Count() {
+
+  const [value, setValue] = useState('')
+  ;
+  return (
+    <div className="container">
+      <div className='calculator'>
+        <form action="">
+          <div className='display'>
+            <input type="text" value={value} />
+          </div>
+
+          <div>
+            <input type="button" value="AC" onClick={e => setValue('')}/>
+            <input type="button" value="DEL" onClick={e => setValue(value.slice(0, -1))}/>
+            <input type="button" value=" " />
+            <input type="button" value="/" />
+          </div>
+
+          <div>
+            <input type="button" value={7} onClick={e => setValue(value + e.target.value)} />
+            <input type="button" value={8} onClick={e => setValue(value + e.target.value)}/>
+            <input type="button" value={9} onClick={e => setValue(value + e.target.value)}/>
+            <input type="button" value="+" />
+
+          </div>
+          <div>
+            <input type="button" value={4} onClick={e => setValue(value + e.target.value)}/>
+            <input type="button" value={5} onClick={e => setValue(value + e.target.value)}/>
+            <input type="button" value={6} onClick={e => setValue(value + e.target.value)}/>
+            <input type="button" value="-" />
+
+          </div>
+          <div>
+            <input type="button" value={1} onClick={e => setValue(value + e.target.value)}/>
+            <input type="button" value={2} onClick={e => setValue(value + e.target.value)}/>
+            <input type="button" value={3} onClick={e => setValue(value + e.target.value)}/>
+            <input type="button" value="×"/>
+          </div>
+          <div>
+            <input type="button" value="." onClick={e => setValue(value + e.target.value)}/>
+            <input type="button" value={0} onClick={e => setValue(value + e.target.value)}/>
+            <input type="button" value=" " />
+            <input type="button" value="=" />
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Count
+
+
+
+
+
