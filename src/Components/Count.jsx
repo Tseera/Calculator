2b18c4258a4798c54203ca55067b338852d9cf74
Tseera <tayseersomane@gmail.com>
2024-04-24 10:18:37 +0300

    changes

--- src/Components/Count.jsx
+++ src/Components/Count.jsx
@@ -4,6 +4,21 @@ import React from 'react'
 import { useState } from 'react';
 
 function Count() {
+  function handleClick() {
+    const result = evaluateExpression(value);
+    setValue(result);
+}
+
+function evaluateExpression(expression) {
+    try {
+        return eval(expression); 
+    } catch (error) {
+        setValue('Error evaluating expression:', error);
+    }
+
+  
+}
+
 
   const [value, setValue] = useState('')
   ;
@@ -18,35 +33,35 @@ function Count() {
           <div>
             <input type="button" value="AC" onClick={e => setValue('')}/>
             <input type="button" value="DEL" onClick={e => setValue(value.slice(0, -1))}/>
-            <input type="button" value=" " />
-            <input type="button" value="/" />
+            <input type="button" value="^" onClick={e => setValue(value + e.target.value)} />
+            <input type="button" value="/" onClick={e => setValue(value + e.target.value)} />
           </div>
 
           <div>
             <input type="button" value={7} onClick={e => setValue(value + e.target.value)} />
             <input type="button" value={8} onClick={e => setValue(value + e.target.value)}/>
             <input type="button" value={9} onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="+" />
+            <input type="button" value="+" onClick={e => setValue(value + e.target.value)}/>
 
           </div>
           <div>
             <input type="button" value={4} onClick={e => setValue(value + e.target.value)}/>
             <input type="button" value={5} onClick={e => setValue(value + e.target.value)}/>
             <input type="button" value={6} onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="-" />
+            <input type="button" value="-" onClick={e => setValue(value + e.target.value)}/>
 
           </div>
           <div>
             <input type="button" value={1} onClick={e => setValue(value + e.target.value)}/>
             <input type="button" value={2} onClick={e => setValue(value + e.target.value)}/>
             <input type="button" value={3} onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="×"/>
+            <input type="button" value="*"onClick={e => setValue(value + e.target.value)}/>
           </div>
           <div>
             <input type="button" value="." onClick={e => setValue(value + e.target.value)}/>
             <input type="button" value={0} onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value=" " />
-            <input type="button" value="=" />
+            <input type="button" value="%" onClick={e => setValue(value + e.target.value)}/>
+            <input type="button" value="=" onClick={handleClick}/>
           </div>
         </form>
       </div>
